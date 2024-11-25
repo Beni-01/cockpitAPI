4dd2bb25e2c79896aa138e9fcad9c57fea38aef0
Beni Mampunina <bm@cocoders.one>
2024-11-25 08:55:27 +0100

    feat:add change

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,6 @@ module.exports = {
       {
         name: "f360api",          // Nom de votre application
         script: "dist/src/main.js",    // Point d'entrée de votre application (après compilation TypeScript)
-        instances: 1,              // Nombre d'instances (1 pour une seule instance, "max" pour utiliser tous les cœurs CPU disponibles)
-        // exec_mode: "cluster",      // Mode cluster pour répartir les charges entre les cœurs (recommandé en production)
         watch: false,               // Désactive le rechargement automatique en production
         max_memory_restart: "300M", // Redémarre l'application si elle dépasse 300 MB de mémoire
         env: {
