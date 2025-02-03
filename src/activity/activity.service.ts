16a6c16148a939f394a1e1108e907a11888e17f8
Beni Mampunina <bm@cocoders.one>
2025-02-03 12:28:32 +0100

    feat:add change

--- src/activity/activity.service.ts
+++ src/activity/activity.service.ts
@@ -276,9 +276,9 @@ export class ActivityService {
                 nextDay.setDate(nextDay.getDate() + 1);
                 queryBuilder.andWhere('(activity.dateDebut BETWEEN :dateDebut AND :dateFin)', { dateDebut, dateFin: nextDay.toISOString() });
                 //queryBuilder.andWhere('activity.dateDebut >= :dateDebut AND activity.dateFin <= :nextDay', { dateDebut, nextDay: nextDay.toISOString() });
-            } else if (dateDebut && !dateFin) {
+            } else if (dateDebut) {
                 queryBuilder.andWhere('activity.dateDebut >= :dateDebut', { dateDebut });
-            } else if (dateFin && !dateDebut) {
+            } else if (dateFin) {
                 const nextDay = new Date(dateFin);
                 nextDay.setDate(nextDay.getDate() + 1);
                 queryBuilder.andWhere('activity.dateFin <= :nextDay', { nextDay: nextDay.toISOString() });
