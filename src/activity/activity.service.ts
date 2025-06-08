bc1d239d7009b27e230dc956ae856ea4d98f4605
Beni Mampunina <bm@cocoders.one>
2025-06-08 22:19:08 +0100

    feat:add change

--- src/activity/activity.service.ts
+++ src/activity/activity.service.ts
@@ -938,7 +938,7 @@ async getDirectionProgressDeepSeek(
                 // KPI5 - Budget
                 totalBudget:stats.totalBudget, 
                 totalBudgetConsomme:stats.totalBudgetConsomme,
-                kpi5_percent: Number(rateBudget.toFixed(2))
+                kpi5_percent:  stats.totalBudgetConsomme==0 ? 0 : Number(rateBudget.toFixed(2))
             };
         });
 
