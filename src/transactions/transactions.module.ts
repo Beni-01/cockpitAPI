26dba8ff30134dea2a5fa20d5331820acc8fdbb0
Beni-01 <contactmampbeni@gmail.com>
2025-12-31 18:12:05 +0100

    feat:add change

--- src/transactions/transactions.module.ts
+++ src/transactions/transactions.module.ts
@@ -3,10 +3,11 @@ import { TransactionsService } from './transactions.service';
 import { TransactionsController } from './transactions.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Transaction } from './entities/transaction.entity';
+import { Dataformater } from 'src/utilities/data-formater.class';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Transaction])],
   controllers: [TransactionsController],
-  providers: [TransactionsService],
+  providers: [TransactionsService, Dataformater],
 })
 export class TransactionsModule {}
