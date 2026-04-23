990263783345099b92e2d58408d8da67d9cd2301
Beni-01 <contactmampbeni@gmail.com>
2026-04-23 12:05:39 +0100

    feat:add change

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,12 +6,13 @@ config();
 export const AppDataSource = new DataSource({
     type: 'mysql',
     host: process.env.DB_HOST || 'localhost',
-    port: parseInt(process.env.DB_PORT) || 3307,
+    port: parseInt(process.env.DB_PORT) || 8009,
     username: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'F360DB',
+    database: process.env.DB_NAME || 'cockpitDB',
     entities: ['src/**/*.entity.ts'],
     migrations: ['src/**/migrations/*.ts'],
     synchronize: false,
     logging: true,
 });
+
