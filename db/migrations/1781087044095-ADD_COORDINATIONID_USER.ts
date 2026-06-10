fc6840ed36ab3f13bd532f0d2301e3551a29238c
Beni-01 <contactmampbeni@gmail.com>
2026-06-10 12:24:08 +0100

    feat:add change

---
+++ db/migrations/1781087044095-ADD_COORDINATIONID_USER.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class ADDCOORDINATIONIDUSER1781087044095 implements MigrationInterface {
+    name = 'ADDCOORDINATIONIDUSER1781087044095'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+
+        await queryRunner.query(`ALTER TABLE \`user\` ADD \`coordinationId\` int NULL`);
+
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+       
+    }
+
+}
